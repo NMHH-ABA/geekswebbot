9a0f7dd92734385c5955ce05393f2d4e73fcebdc
NMHH-ABA <NMHH.ABA@gmail.com>
2020-02-17 14:18:09 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <?php
-require 'vendor/autoload.php';
+include 'vendor/autoload.php';
+include 'vendor/Telegram.php';
+include_once 'vendor/jdf.php');
 
 $telegram = new Zelenin\Telegram\Bot\Api('821293043:AAGV87SgJErV0yQm3np9uvZ3WXKvViX0E10'); // Set your access token
 $update = json_decode(file_get_contents('php://input'));
 $chat_id = $update->message->chat->id;
+$textoriginal = $update->message->text;
+$text = strtolower($textoriginal);
 
 //your app
-try {
-
-    if($update->message->text == '/start')
+ if($text == '/start')
     {
 
-    	$response = $telegram->sendMessage([
-        	'chat_id' => $chat_id,
-        	'text' => "Bot is Online!"
-     	]);
+        $response = $telegram->sendMessage([
+            'chat_id' => $chat_id,
+            'text' => "Bot is Online!"
+        ]);
     }
     else
     {
-    	$response = $telegram->sendMessage([
-    		'chat_id' => $chat_id,
-    		'text' => "Invalid command"
-    		]);
+        $response = $telegram->sendMessage([
+            'chat_id' => $chat_id,
+            'text' => "Invalid command"
+            ]);
     }
-
-}
