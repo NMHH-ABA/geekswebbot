8c0734637201af562d9973e891f1a6f30b31dcc3
NMHH-ABA <NMHH.ABA@gmail.com>
2021-03-21 14:35:34 +0330

    Update index.php

--- index.php
+++ index.php
@@ -373,8 +373,9 @@ else if ( stristr ( $text , 'days' ) == TRUE )
         $Parts0 = explode ( '-' , $Day0 , 3 );
         $TimeStamp0 = mktime ( 0 , 0 , 0 , $Parts0[ 1 ] , $Parts0[ 2 ] , $Parts0[ 0 ] );
         $dayharfi0 = jgetdate ( $TimeStamp0 );
-        $dateharfi0 = tr_num ( ( $dayharfi0[ weekday ] . " " . $dayharfi0[ mday ] . " " . $dayharfi0[ month ] . " " . $dayharfi0[ year ] ) , 'fa' );
         $telegram->sendMessage(['chat_id' => $chat_id, 'text' => "$dayharfi0"]);
+        $dateharfi0 = tr_num ( ( $dayharfi0[ weekday ] . " " . $dayharfi0[ mday ] . " " . $dayharfi0[ month ] . " " . $dayharfi0[ year ] ) , 'fa' );
+        
 
 
         $day1 = date_create ( date ( "Y-m-d" ) );
